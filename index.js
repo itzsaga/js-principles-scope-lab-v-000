504034df16bd9ca6752f61d46e563502ea1ed31a
Seth Alexander <seth.alexander@wework.com>
2018-03-07 01:17:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 
 var customerName = 'bob';
-const leastFavoriteCustomer;
+const leastFavoriteCustomer = 'you';
 
 function upperCaseCustomerName() {
   return customerName = customerName.toUpperCase();
